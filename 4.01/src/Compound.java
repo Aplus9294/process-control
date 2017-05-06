825b9cc45775930d1d2b0fbf72b244ee5926c3e6
Administrator <Administrator@A9294Computer>
2017-05-06 23:54:23 +0800

    Change-Id: I616e7e2fca07c479bd47f4662175ec03c7eed7e1 Signed-off-by: Administrator <Administrator@A9294Computer>

---
+++ 4.01/src/Compound.java
@@ -0,0 +1,16 @@
+public class Compound {
+	public static void main(String args[]) {
+		{
+			int y = 40;
+			System.out.println(y);
+			int z = 245;
+			boolean b;
+			{
+				b = y > z;
+				System.out.println(b); // ∏¥∫œ”Ôæ‰
+			}
+		}
+		String word = "hello java";
+		System.out.println(word);
+	}
+}
